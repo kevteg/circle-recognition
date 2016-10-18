febda5393a802293588edfaf2166e751b9190562
Juan Molero <juanmolerog@gmail.com>
2016-10-18 12:26:35 -0400

    Carga de samples

--- neural-network-matlab/loadSamples.m
+++ neural-network-matlab/loadSamples.m
@@ -1,12 +1,15 @@
 function [X,Y] = loadSamples(num)
   system("rm matrix/*.mat");
-  system(["python input/generate.py -i 1.jpg -n ", num2str(num)]);
-  X1 = load("matrix/0-input.mat","image_input");
-  Y1 = load("matrix/0-output.mat","image_output");
+  system(["python3 input/generate.py -i 1.jpg -n ", num2str(num)]);
+  X1 = load("matrix/0-input.mat","image_input").image_input;
+  X1 = X1(:);
+  Y1 = load("matrix/0-output.mat","image_output").image_output;
+  Y1 = Y1(:,:,1);
+  Y1 = Y1(:);
   X = zeros(num,size(X1),"uint8");
   Y = zeros(num,size(Y1),"uint8");
   for n=1:num
-    X(n,:) = load(["matrix/" num2str(n-1) "-input.mat"],"image_input");
-    Y(n,:) = load(["matrix/" num2str(n-1) "-output.mat"],"image_output");
+    X(n,:) = load(["matrix/" num2str(n-1) "-input.mat"],"image_input").image_input(:);
+    Y(n,:) = load(["matrix/" num2str(n-1) "-output.mat"],"image_output").image_output(:,:,1)(:);
   endfor
 end
