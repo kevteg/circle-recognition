16dca372a8d3a7e69e999dbad8e48ab79d314008
Juan Molero <juanmolerog@gmail.com>
2016-11-02 02:00:23 -0400

    pruebas de costo

--- neural-network-matlab/costFunction.m
+++ neural-network-matlab/costFunction.m
@@ -1,4 +1,14 @@
 function [J grad] = costFunction(nn_params,inX,inY,outX,outy,hidden_layers,...
                                  X,Y,lambda)
+
+if hidden_layers > 1
+	Theta = zeros(hidden_layers-1,inX,inY);
+	for n=1:hidden_layers-1
+		Theta(n,:,:) = reshape(nn_params(1+(inX*inY*(n-1)+1):inX*inY*n),inX*inY,inX*inY+1);
+	end
+end
+
+FinalTheta = reshape(nn_params(end-:end))
+
 end
     
